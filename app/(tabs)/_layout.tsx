5ebbbb1f5457ab2df32701200f39d11d78d32902
alim628 <1054492051@qq.com>
2023-03-15 21:36:28 +0800

    icon style

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs } from "expo-router";
 import { Pressable, useColorScheme } from "react-native";
-
+import { StyleSheet } from "react-native";
 import Colors from "../../constants/Colors";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
@@ -22,14 +22,14 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
         name="parag"
         options={{
-          title: "parag",
           tabBarIcon: ({ color }) => (
-            <Ionicons name="chatbubble" color={color} />
+            <Ionicons name="chatbubble" size="large" color={color} />
           ),
         }}
       />
@@ -37,23 +37,37 @@ export default function TabLayout() {
         name="doslar"
         options={{
           title: "doslar",
-          tabBarIcon: ({ color }) => <Ionicons name="people" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="people" size="large" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="compas"
         options={{
           title: "compas",
-          tabBarIcon: ({ color }) => <Ionicons name="compass" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="compass" size="large" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="man"
         options={{
           title: "man",
-          tabBarIcon: ({ color }) => <Ionicons name="person" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="person" size="large" color={color} />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
