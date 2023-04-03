197b70392aae548cee69619c74712e3a0efa7e78
alim628 <1054492051@qq.com>
2023-04-03 21:18:43 +0800

    paraglixix betining kurunma yuzi tugidi

--- app/(tabs)/parag.tsx
+++ app/(tabs)/parag.tsx
@@ -1,37 +1,85 @@
-import { Image, StyleSheet } from "react-native";
+import { Image, StyleSheet, SafeAreaView, FlatList } from "react-native";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
 export default function ParagScreen() {
-  return (
+  const DATA = [
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Ahmat', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Samat', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Adil', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+    { isim: 'Alim', rasim: './../../assets/baxsurat/微信图片_20230315215134.jpg' },
+  ]
+  const renderItem = ({ item }) => (
     <View style={styles.container}>
       <View style={styles.dos}>
         <Image
           style={styles.baxsurat}
-          source={require("./../../assets/baxsurat/微信图片_20230315215134.jpg")}
+          source={item.rasim}
         ></Image>
-        <Text>Alim</Text>
+        <View>
+          <Text>{item.isim}</Text>
+          <Text style={styles.hat}>{item.isim}</Text>
+        </View>
       </View>
     </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+
   },
   baxsurat: {
     height: 50,
     width: 50,
     marginRight: 10,
+    borderRadius: 10
   },
   dos: {
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
     flexDirection: "row",
+    marginLeft: 10,
+    marginTop: 10
   },
+  hat:{
+    fontSize:4,
+    color:'#56585d'
+  }
 });
